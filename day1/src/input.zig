e02429211c27b5d8a58fd6692b6959669bdd2128
Roy van de Water <roy.v.water@gmail.com>
2022-12-18 13:28:24 -0700

    Input has been parsed

--- day1/src/input.zig
+++ day1/src/input.zig
@@ -10,6 +10,10 @@ const Input = struct {
         return self.elves.items.len;
     }
 
+    pub fn next(self: *Input) ?Elf {
+        return self.elves.popOrNull();
+    }
+
     pub fn deinit(self: Input) void {
         for (self.elves.items) |elf| {
             elf.deinit();
@@ -39,18 +43,60 @@ pub fn parseInput(allocator: std.mem.Allocator, reader: anytype) !Input {
 
     return Input{
         .allocator = allocator,
-        .elves = elves,
+        .elves = try reverseList(Elf, &elves),
     };
 }
 
+fn reverseList(comptime T: type, list: *std.ArrayList(T)) !std.ArrayList(T) {
+    var new_list = try std.ArrayList(T).initCapacity(list.allocator, list.items.len);
+
+    while (list.popOrNull()) |item| {
+        try new_list.append(item);
+    }
+
+    defer list.deinit();
+    return new_list;
+}
+
 const test_allocator = std.testing.allocator;
+const expect = std.testing.expect;
+
 test "parse one elf" {
     var br = std.io.fixedBufferStream(
         \\1000
         \\2000
     );
-    const input = try parseInput(test_allocator, br.reader());
+    var input = try parseInput(test_allocator, br.reader());
     defer input.deinit();
 
-    std.debug.assert(1 == input.len());
+    try expect(1 == input.len());
+
+    const elf = input.next().?;
+    defer elf.deinit();
+    try expect(std.mem.eql(usize, elf.items, &[_]usize{1000, 2000}));
+
+}
+
+test "parse two elves" {
+    var br = std.io.fixedBufferStream(
+        \\3000
+        \\1000
+        \\
+        \\4000
+    );
+    var input = try parseInput(test_allocator, br.reader());
+    defer input.deinit();
+
+    try expect(2 == input.len());
+
+    const elf_1 = input.next().?;
+    defer elf_1.deinit();
+    try expect(std.mem.eql(usize, elf_1.items, &[_]usize{3000, 1000}));
+
+    const elf_2 = input.next().?;
+    defer elf_2.deinit();
+    try expect(std.mem.eql(usize, elf_2.items, &[_]usize{4000}));
+
+    try expect(null == input.next());
+
 }
