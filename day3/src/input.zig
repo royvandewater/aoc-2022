0f83b377e6e78cce22a1df26e1a0bbfc18809319
Roy van de Water <roy.v.water@gmail.com>
2022-12-24 10:45:51 -0700

    Util functions

--- day3/src/input.zig
+++ day3/src/input.zig
@@ -8,30 +8,58 @@ pub const Input = struct {
     allocator: Allocator,
     rucksacks: []Rucksack,
 
+    pub fn fromSlice(allocator: Allocator, slice: []Rucksack) !Input {
+        var rucksacks = try allocator.alloc(Rucksack, slice.len);
+
+        for (slice) |rucksack, i| {
+            rucksacks[i] = Rucksack{
+                try copy(allocator, rucksack[0]),
+                try copy(allocator, rucksack[1]),
+            };
+        }
+
+        return Input{
+            .allocator = allocator,
+            .rucksacks = rucksacks,
+        };
+    }
+
+    pub fn fromString(allocator: Allocator, str: []const u8) !Input {
+        var rucksacks = std.ArrayList(Rucksack).init(allocator);
+
+        var lines = std.mem.split(u8, str, "\n");
+        while (lines.next()) |line| {
+            if (line.len == 0) continue;
+
+            const n = line.len / 2;
+            try rucksacks.append(Rucksack{
+                try copy(allocator, line[0..n]),
+                try copy(allocator, line[n..]),
+            });
+        }
+
+        return Input{
+            .allocator = allocator,
+            .rucksacks = rucksacks.toOwnedSlice(),
+        };
+    }
+
     pub fn fromReader(allocator: Allocator, reader: anytype) !Input {
         var rucksacks = std.ArrayList(Rucksack).init(allocator);
 
         var buf: [128]u8 = undefined;
         while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
             const n = line.len / 2;
-
-            var first = try allocator.alloc(u8, n);
-            std.mem.copy(u8, first, line[0..n]);
-
-            var second = try allocator.alloc(u8, n);
-            std.mem.copy(u8, second, line[n..]);
-
-            const rucksack = Rucksack{ first, second };
-
-            try rucksacks.append(rucksack);
+            try rucksacks.append(Rucksack{
+                try copy(allocator, line[0..n]),
+                try copy(allocator, line[n..]),
+            });
         }
 
-        var i = Input{
+        return Input{
             .allocator = allocator,
             .rucksacks = rucksacks.toOwnedSlice(),
         };
-
-        return i;
     }
 
     pub fn deinit(self: Input) void {
@@ -43,6 +71,12 @@ pub const Input = struct {
         self.allocator.free(self.rucksacks);
     }
 };
+
+fn copy(allocator: Allocator, str: []const u8) ![]const u8 {
+    var buf = try allocator.alloc(u8, str.len);
+    std.mem.copy(u8, buf, str);
+    return buf;
+}
 
 const test_allocator = std.testing.allocator;
 const expectEqual = std.testing.expectEqual;
@@ -59,6 +93,41 @@ test "fromReader empty" {
 test "fromReader one rucksack" {
     var br = std.io.fixedBufferStream("abc123");
     var input = try Input.fromReader(test_allocator, br.reader());
+    defer input.deinit();
+
+    try expectEqual(@as(usize, 1), input.rucksacks.len);
+    try expectEqualStrings("abc", input.rucksacks[0][0]);
+    try expectEqualStrings("123", input.rucksacks[0][1]);
+}
+
+test "fromSlice empty" {
+    var rucksacks = [_]Rucksack{};
+    var input = try Input.fromSlice(test_allocator, &rucksacks);
+    defer input.deinit();
+
+    try expectEqual(@as(usize, 0), input.rucksacks.len);
+}
+
+test "fromSlice one rucksack" {
+    var rucksacks = [_]Rucksack{Rucksack{"abc","123"}};
+    var input = try Input.fromSlice(test_allocator, &rucksacks);
+    defer input.deinit();
+
+    try expectEqual(@as(usize, 1), input.rucksacks.len);
+    try expectEqualStrings("abc", input.rucksacks[0][0]);
+    try expectEqualStrings("123", input.rucksacks[0][1]);
+}
+
+
+test "fromString empty" {
+    var input = try Input.fromString(test_allocator, "");
+    defer input.deinit();
+
+    try expectEqual(@as(usize, 0), input.rucksacks.len);
+}
+
+test "fromString one rucksack" {
+    var input = try Input.fromString(test_allocator, "abc123");
     defer input.deinit();
 
     try expectEqual(@as(usize, 1), input.rucksacks.len);
