0b64148b54c8c71849a9e5b762238a9968007ccb
Roy van de Water <roy.v.water@gmail.com>
2022-12-25 07:44:56 -0700

    Day 4, part 2

--- day4/src/main.zig
+++ day4/src/main.zig
@@ -14,6 +14,7 @@ pub fn main() !void {
     const stdout = bw.writer();
 
     try stdout.print("Stage 1: {any}\n", .{try stage1(input)});
+    try stdout.print("Stage 2: {any}\n", .{try stage2(input)});
 
     try bw.flush(); // don't forget to flush!
 }
@@ -28,7 +29,23 @@ fn stage1(str: []const u8) !usize {
         if (line.len == 0) continue;
         const ranges = try parseRanges(line);
 
-        if (totalRangeOverlap(ranges[0], ranges[1]) or totalRangeOverlap(ranges[1], ranges[0])) {
+        if (totalRangeOverlap(ranges[0], ranges[1])) {
+            total += 1;
+        }
+    }
+
+    return total;
+}
+
+fn stage2(str: []const u8) !usize {
+    var total: usize = 0;
+
+    var lines = std.mem.split(u8, str, "\n");
+    while (lines.next()) |line| {
+        if (line.len == 0) continue;
+        const ranges = try parseRanges(line);
+
+        if (partialRangeOverlap(ranges[0], ranges[1])) {
             total += 1;
         }
     }
@@ -37,7 +54,26 @@ fn stage1(str: []const u8) !usize {
 }
 
 fn totalRangeOverlap(range_1: Range, range_2: Range) bool {
-    return range_1[0] <= range_2[0] and range_1[1] >= range_2[1];
+    const range_1_overlaps_2 = range_1[0] <= range_2[0] and range_1[1] >= range_2[1];
+    const range_2_overlaps_1 = range_2[0] <= range_1[0] and range_2[1] >= range_1[1];
+
+    return range_1_overlaps_2 or range_2_overlaps_1;
+}
+
+fn partialRangeOverlap(range_1: Range, range_2: Range) bool {
+    if (totalRangeOverlap(range_1, range_2)) {
+        return true;
+    }
+
+    if (range_2[0] <= range_1[0] and range_2[1] >= range_1[0]) {
+        return true;
+    }
+
+    if (range_1[0] <= range_2[0] and range_1[1] >= range_2[0]) {
+        return true;
+    }
+
+    return false;
 }
 
 fn parseRanges(line: []const u8) ![2]Range {
@@ -72,4 +108,22 @@ test "stage 1: example 1" {
     );
 
     try std.testing.expectEqual(@as(usize, 2), answer);
+}
+
+test "stage 2: empty" {
+    const answer = try stage2("");
+    try std.testing.expectEqual(@as(usize, 0), answer);
+}
+
+test "stage 2: example 1" {
+    const answer = try stage2(
+        \\2-4,6-8
+        \\2-3,4-5
+        \\5-7,7-9
+        \\2-8,3-7
+        \\6-6,4-6
+        \\2-6,4-8
+    );
+
+    try std.testing.expectEqual(@as(usize, 4), answer);
 }
