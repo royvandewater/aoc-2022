30ec032873e4dbc1d17b1089257db53d97e19cb5
Roy van de Water <roy.v.water@gmail.com>
2022-12-28 09:20:54 -0700

    Day 5, parse input

--- day5/src/input.zig
+++ day5/src/input.zig
@@ -2,12 +2,32 @@ const std = @import("std");
 
 const Allocator = std.mem.Allocator;
 
-pub const Rucksack = [2][]const u8;
+pub const Instruction = struct {
+    quantity: usize,
+    from: usize,
+    to: usize,
+
+    pub fn fromString(str: []const u8) !Instruction {
+        var parts = std.mem.split(u8, str, " ");
+        _ = parts.next(); // "move"
+        const quantity = try std.fmt.parseUnsigned(usize, parts.next().?, 10);
+        _ = parts.next(); // "from"
+        const from = try std.fmt.parseUnsigned(usize, parts.next().?, 10);
+        _ = parts.next(); // "to"
+        const to = try std.fmt.parseUnsigned(usize, parts.next().?, 10);
+
+        return Instruction{
+            .quantity = quantity,
+            .from = from,
+            .to = to,
+        };
+    }
+};
 
 pub const Input = struct {
     allocator: Allocator,
     stacks: std.AutoHashMap(usize, std.ArrayList(u8)),
-    instructions: [][]const u8,
+    instructions: []Instruction,
 
     pub fn deinit(self: *Input) void {
         var stacks = self.stacks.valueIterator();
@@ -16,15 +36,12 @@ pub const Input = struct {
         }
         self.stacks.deinit();
 
-        // for (self.instructions) |instruction| {
-        //     self.allocator.free(instruction);
-        // }
         self.allocator.free(self.instructions);
     }
 
     pub fn fromReader(allocator: Allocator, reader: anytype) !Input {
         var stacks = std.AutoHashMap(usize, std.ArrayList(u8)).init(allocator);
-        var instructions = std.ArrayList([]const u8).init(allocator);
+        var instructions = std.ArrayList(Instruction).init(allocator);
 
         var buf: [128]u8 = undefined;
         while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
@@ -42,6 +59,11 @@ pub const Input = struct {
                 }
                 try stacks.getPtr(x).?.append(char);
             }
+        }
+
+        while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+            if (line.len == 0) continue;
+            try instructions.append(try Instruction.fromString(line));
         }
 
         return Input{
@@ -68,25 +90,11 @@ pub const Input = struct {
     //     };
     // }
 
-    // pub fn fromString(allocator: Allocator, str: []const u8) !Input {
-    //     var rucksacks = std.ArrayList(Rucksack).init(allocator);
+    pub fn fromString(allocator: Allocator, str: []const u8) !Input {
+        var br = std.io.fixedBufferStream(str);
 
-    //     var lines = std.mem.split(u8, str, "\n");
-    //     while (lines.next()) |line| {
-    //         if (line.len == 0) continue;
-
-    //         const n = line.len / 2;
-    //         try rucksacks.append(Rucksack{
-    //             try copy(allocator, line[0..n]),
-    //             try copy(allocator, line[n..]),
-    //         });
-    //     }
-
-    //     return Input{
-    //         .allocator = allocator,
-    //         .rucksacks = rucksacks.toOwnedSlice(),
-    //     };
-    // }
+        return Input.fromReader(allocator, br.reader());
+    }
 };
 
 // fn copy(allocator: Allocator, str: []const u8) ![]const u8 {
@@ -118,39 +126,42 @@ test "fromReader one stack with one crate" {
 
     try expectEqual(@as(usize, 1), input.stacks.count());
     try expectEqual(@as(usize, 1), input.stacks.get(1).?.items.len);
+    try expectEqual(@as(u8, 'A'), input.stacks.get(1).?.items[0]);
 }
 
-// test "fromSlice empty" {
-//     var rucksacks = [_]Rucksack{};
-//     var input = try Input.fromSlice(test_allocator, &rucksacks);
-//     defer input.deinit();
+test "fromReader one stack with one instruction" {
+    var br = std.io.fixedBufferStream(
+        \\[A] [B]
+        \\ 1   2
+        \\
+        \\move 1 from 2 to 3
+    );
+    var input = try Input.fromReader(test_allocator, br.reader());
+    defer input.deinit();
 
-//     try expectEqual(@as(usize, 0), input.rucksacks.len);
-// }
+    try expectEqual(@as(usize, 1), input.instructions.len);
+    try expectEqual(@as(usize, 1), input.instructions[0].quantity);
+    try expectEqual(@as(usize, 2), input.instructions[0].from);
+    try expectEqual(@as(usize, 3), input.instructions[0].to);
+}
 
-// test "fromSlice one rucksack" {
-//     var rucksacks = [_]Rucksack{Rucksack{"abc","123"}};
-//     var input = try Input.fromSlice(test_allocator, &rucksacks);
-//     defer input.deinit();
+test "fromString" {
+    var input = try Input.fromString(test_allocator,
+        \\[A] [B]
+        \\ 1   2
+        \\
+        \\move 1 from 2 to 3
+    );
+    defer input.deinit();
 
-//     try expectEqual(@as(usize, 1), input.rucksacks.len);
-//     try expectEqualStrings("abc", input.rucksacks[0][0]);
-//     try expectEqualStrings("123", input.rucksacks[0][1]);
-// }
+    try expectEqual(@as(usize, 2), input.stacks.count());
+    try expectEqual(@as(usize, 1), input.stacks.get(1).?.items.len);
+    try expectEqual(@as(u8, 'A'), input.stacks.get(1).?.items[0]);
+    try expectEqual(@as(usize, 1), input.stacks.get(2).?.items.len);
+    try expectEqual(@as(u8, 'B'), input.stacks.get(2).?.items[0]);
 
-
-// test "fromString empty" {
-//     var input = try Input.fromString(test_allocator, "");
-//     defer input.deinit();
-
-//     try expectEqual(@as(usize, 0), input.rucksacks.len);
-// }
-
-// test "fromString one rucksack" {
-//     var input = try Input.fromString(test_allocator, "abc123");
-//     defer input.deinit();
-
-//     try expectEqual(@as(usize, 1), input.rucksacks.len);
-//     try expectEqualStrings("abc", input.rucksacks[0][0]);
-//     try expectEqualStrings("123", input.rucksacks[0][1]);
-// }
+    try expectEqual(@as(usize, 1), input.instructions.len);
+    try expectEqual(@as(usize, 1), input.instructions[0].quantity);
+    try expectEqual(@as(usize, 2), input.instructions[0].from);
+    try expectEqual(@as(usize, 3), input.instructions[0].to);
+}
